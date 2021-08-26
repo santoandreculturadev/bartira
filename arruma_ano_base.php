74734f9aa54955ff57164de2f6cb14ffd376b54e
Marcio Yonamine <marcioyonamine@gmail.com>
2021-08-26 11:39:40 -0300

    arruma ano base

--- arruma_ano_base.php
+++ arruma_ano_base.php
@@ -21,12 +21,12 @@
 		$sql = "SELECT projeto, ficha, ano_base FROM sc_orcamento WHERE id = '$dotacao'";
         $res = $wpdb->get_results($sql,ARRAY_A);		
 		for($i = 0; $i < count($res); $i++){
-			if($res[$i]['ano_base'] == '2021'){
+			if($res[$i]['ano_base'] == '2020'){
 				$x['status'] = 'correto';
 			}
-			if($res[$i]['ano_base'] == '2020'){
-				$sql_retorna_2021 = "SELECT id FROM sc_orcamento WHERE projeto = '".$res[$i]['projeto']."' AND ficha = '".$res[$i]['ficha']."' AND ano_base = '2021'";
-				 $res2 = $wpdb->get_results($sql_retorna_2021,ARRAY_A);
+			if($res[$i]['ano_base'] == '2021'){
+				$sql_retorna_2020 = "SELECT id FROM sc_orcamento WHERE projeto = '".$res[$i]['projeto']."' AND ficha = '".$res[$i]['ficha']."' AND ano_base = '2020'";
+				 $res2 = $wpdb->get_results($sql_retorna_2020,ARRAY_A);
 				
 				if($res2 != NULL){
 					$x['eq'] = $res2[0]['id'];
@@ -43,18 +43,45 @@
 	}
 
 
+	//cria backup
+	$datebackup = date('YmdHis');
+	$sql_backup01 = "CREATE TABLE sc_mov_orc_".$datebackup." SELECT * FROM sc_mov_orc;";
+	$sql_backup02 = "ALTER TABLE sc_mov_orc_".$datebackup." ADD PRIMARY KEY(`id`)";
+	$sql_backup03 = "ALTER TABLE sc_mov_orc_".$datebackup." MODIFY COLUMN id auto_increment;";
+	
+	$backup = false;
+	
+	$backup = $wpdb->query($sql_backup01);
+	$wpdb->query($sql_backup02);
+	$wpdb->query($sql_backup03);
+	
+	var_dump($backup);
+	if($backup == true){
 
-   $sql = "SELECT * FROM sc_mov_orc WHERE data BETWEEN '2021-01-01' AND '2021-12-31' AND publicado = '1'";
+
+
+   $sql = "SELECT * FROM sc_mov_orc WHERE data BETWEEN '2020-01-01' AND '2020-12-31' AND publicado = '1'";
    $res = $wpdb->get_results($sql,ARRAY_A);
    for($i = 0; $i < count($res); $i++){
 		//echo $res[$i]['dotacao']."<br />";
 		$x = dotacaoArruma($res[$i]['dotacao']);
 		$texto = "A movimentacao ".$res[$i]['id']." está ".$x['status'];
 		if($x['status'] == 'errado'){
-			$sql_atualiza = "UPDATE sc_mov_orc SET tipo = '".$x['eq']."' WHERE id = '".$res[$i]['id']."'";
+			$sql_atualiza = "UPDATE sc_mov_orc SET dotacao = '".$x['eq']."' WHERE id = '".$res[$i]['id']."'";
 			$exec = $wpdb->query($sql_atualiza);
 			if($exec){
 				$texto .= ". Atualização foi feita com sucesso";
+				if($res[$i]['idPedidoContratacao'] != 0){
+					$sql_atualiza_pedido = "UPDATE sc_contratacao SET dotacao = '".$x['eq']."' WHERE idPedidoContratacao = '".$res[$i]['idPedidoContratacao']."'";
+					$exec2 = $wpdb->query($sql_atualiza_pedido);
+					if($exec2){
+						$texto .= "Atualização do pedido foi feita com sucesso tb";
+					}else{
+						$texto .= "Atualizacao do pedido deu merda.Chora.";
+					}
+				}
+				
+				
 				
 			}else{
 				$texto .= ". Erro na atualização";
@@ -66,7 +93,7 @@
 	
 	}
 	
-  
+	}
   
   ?>
   
