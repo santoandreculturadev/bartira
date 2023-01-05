448e1790c6cb03e3ee099a2e8fa3f184596353eb
santoandreculturadev <santoandrecultura@gmail.com>
2023-01-05 13:31:00 -0300

    ano_base

--- evento.php
+++ evento.php
@@ -704,7 +704,7 @@ $idUser = $user->ID;
 	// Inserir evento
 if(isset($_POST['inserir'])){
 	$sql = "INSERT INTO `sc_evento` (`idEvento`, `idTipo`, `idPrograma`, `idProjeto`, `idLinguagem`, `nomeEvento`, `idResponsavel`, `idSuplente`, `nomeGrupo`, `fichaTecnica`, `faixaEtaria`, `sinopse`, `releaseCom`, `publicado`, `idUsuario`, `linksCom`, `subEvento`, `dataEnvio`, `planejamento`, `inscricao`, `convocatoria_edital` ,`pInterno` , `idRespAprovacao`, `status` , `previsto`, `descricao`,`ano_base`,`online`, `url`, `artista_local`, `cidade`, `n_agentes`,  `n_agentes_abc` ) 
-	VALUES (NULL, '$tipo_evento', '$programa', '$projeto', '$linguagem', '$nomeEvento', '$nomeResponsavel', '$suplente', '$nomeGrupo', '$fichaTecnica', '$faixaEtaria', '$sinopse', '$releaseCom', '1', '$idUser', '$linksCom', 'subEvento', NULL, '$planejamento','$inscricao', '$edital','$p_interno', '$id_aprovacao','1','$previsto','$descricao','2021','$online','$urlonline','$artista_local','$cidade','$n_agentes','$n_agentes_abc')";
+	VALUES (NULL, '$tipo_evento', '$programa', '$projeto', '$linguagem', '$nomeEvento', '$nomeResponsavel', '$suplente', '$nomeGrupo', '$fichaTecnica', '$faixaEtaria', '$sinopse', '$releaseCom', '1', '$idUser', '$linksCom', 'subEvento', NULL, '$planejamento','$inscricao', '$edital','$p_interno', '$id_aprovacao','1','$previsto','$descricao','$ano_base','$online','$urlonline','$artista_local','$cidade','$n_agentes','$n_agentes_abc')";
 	$ins = $wpdb->query($sql);
 	if($ins){
 		$mensagem = "Inserido com sucesso";
@@ -1108,7 +1108,7 @@ $event = evento($_SESSION['id']);
 		<div class="row">    
 			<div class="col-md-offset-2 col-md-8">
 				<h1>Pedidos de Contratação</h1>
-				<p>Para que as informações de Empenho e Pagamento sejam visualizadas, é preciso que seja inserido o número do processo (XXXXX/2020). </p>
+				<p>Para que as informações de Empenho e Pagamento sejam visualizadas, é preciso que seja inserido o número do processo (XXXXX/2023). </p>
 				<p><?php if(isset($mensagem)){ echo $mensagem; }?></p>
 			</div>
 		</div>
