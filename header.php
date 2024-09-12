851d5ff97e7d77599aabc2a6dddb7f07f37fa699
Marcio Yonamine <marcioyonamine@gmail.com>
2024-09-12 16:42:26 -0300

    nova cara

--- header.php
+++ header.php
@@ -24,15 +24,44 @@ require "inc/function.php";
 	<meta name="author" content="">
 	<link rel="icon" href="favicon.ico">
 
-	<title><?php echo $GLOBALS['site_title']; ?></title>
 
 	<!-- Bootstrap core CSS -->
+<!--
 	<link href="dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/scpsa.css" rel="stylesheet">
-
+-->
 	<!-- Custom styles for this template -->
-	<link href="css/dashboard.css" rel="stylesheet">
+<!--	<link href="css/dashboard.css" rel="stylesheet">
 	<script src="js/jquery-3.2.1.js"></script>
 	<link href="css/jquery.floatingscroll.css" rel="stylesheet" type="text/css">
+-->
+	<title><?php echo $GLOBALS['site_title']; ?></title>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+
+
+
+        <!-- Custom fonts for this template-->
+        <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+        <!-- Custom styles for this template-->
+        <link href="./css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="./css/style.css" rel="stylesheet">
+        
+        <link href="./vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="./vendor/jquery/jquery.min.js"></script>
+        <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        
+    <link rel="shortcut icon" href="../img/icone1.png" type="image/x-icon">
+    <link rel="icon" href="./img/icone1.ico" type="image/x-icon">
+
+
 
 </head>
--- menu/me_inicio.php
+++ menu/me_inicio.php
@@ -1,4 +1,108 @@
-<?php include "barra.php"; ?>
+
+            <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                <!-- Sidebar - Brand -->
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+
+                    <div class="sidebar-brand-text mx-3">Administrador</div>
+                </a>
+<?php 
+
+$per = opcaoDados('usuario',$user->ID);
+
+function cmp($a, $b) {
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+				
+$ordem = uasort($per['modulos'],'cmp');
+
+?>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider my-0">
+
+
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Cadastros
+                </div>
+
+
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+					
+					
+                        <i class="fas fa-users"></i>
+                        <span>Módulos</span>
+                    </a>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+  <?php 				
+foreach($per['modulos'] as $modular){
+				$modulo = retornaModulo($modular);
+?>                         
+                            <a class="collapse-item" href="<?php echo $modulo['descricao']; ?>.php"><?php echo $modulo['tipo']; ?>	</a>
+<?php } ?>							
+                        </div>
+                    </div>
+                </li>
+
+                <!-- Nav Item - Utilities Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                        <i class="fas fa-home"></i>
+                        <span>Turmas / Disciplinas</span>
+                    </a>
+                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                           
+                            <a class="collapse-item" href="index.php?pag=<?php echo $menu5 ?>">Cursos</a>
+                            <a class="collapse-item" href="index.php?pag=<?php echo $menu6 ?>">Projeto</a>
+                            <a class="collapse-item" href="index.php?pag=<?php echo $menu7 ?>">Turmas</a>
+                             <a class="collapse-item" href="index.php?pag=<?php echo $menu9 ?>">Diciplinas</a>
+
+                        </div>
+                    </div>
+                </li>
+
+                 <li class="nav-item">
+                    <a class="nav-link" href="../painel-secretaria" target="_blank">
+                        <i class="fas fa-phone-alt fa-chart-area"></i>
+                        <span>Painel Secretaria</span></a>
+                </li>
+                <!--
+
+                 <li class="nav-item">
+                    <a class="nav-link" href="../painel-tesoureiro" target="_blank">
+                        <i class="fas fa-dollar-sign fa-chart-area"></i>
+                        <span>Painel Tesouraria</span></a>
+                </li>
+                       -->
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+              
+              
+
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
+
+                <!-- Sidebar Toggler (Sidebar) -->
+                <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
+
+            </ul>
+
+<?php //include "barra.php"; ?>
 
 
 <div class="container-fluid">
@@ -13,24 +117,8 @@
 
 			
 			<ul class="nav nav-pills flex-column">
-				<?php 
-				$per = opcaoDados('usuario',$user->ID);
 				
-				$n_modulos = (is_array($per['modulos']) ? count($per['modulos']) : 0);
-				
-				for($i = 0; $i < $n_modulos ; $i++){
-					$modulo = retornaModulo($per['modulos'][$i]);
-					
-					?>
-					<li class="nav-item">
-						<a class="nav-link" href="<?php echo $per['modulos'][$i]; ?>.php"><?php echo $modulo['tipo']; ?></a>
-					</li>
-					<?php 
-				}
-				?>
-			</ul>
-
-			<ul class="nav nav-pills flex-column">
+<ul class="nav nav-pills flex-column">
 				<li class="nav-item">
 					<a class="nav-link" href="../wp-login.php?action=logout">Sair</a>
 				</li>
